f009544123e15ad9d41a3b23d94079fc8e668734
ArtemK123 <darkmode1012@gmail.com>
2020-11-21 19:08:21 +0200

    Added ModifyTaskTest

---
+++ todos/TodosTests/TodosTests/TodosPageObject.cs
@@ -0,0 +1,7 @@
+﻿namespace TodosTests
+{
+    internal class TodosPageObject
+    {
+
+    }
+}
--- todos/TodosTests/TodosTests/TodosTests.cs
+++ todos/TodosTests/TodosTests/TodosTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using Xunit;
 
@@ -11,7 +13,7 @@ namespace TodosTests
     public class HelloTodosTest
     {
         [Fact]
-        internal void AddItemTest()
+        internal void AddTaskTest()
         {
             using IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl(@"http://todomvc.com/examples/angularjs/#/");
@@ -37,6 +39,46 @@ namespace TodosTests
 
             Assert.Equal(newTaskTitle, firstTaskTitle);
             Assert.Equal(1, int.Parse(activeItemsCount));
+        }
+
+        [Fact]
+        internal void ModifyTaskTest()
+        {
+            using IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(@"http://todomvc.com/examples/angularjs/#/");
+
+            IWebElement newTaskInput = new WebDriverWait(driver, TimeSpan.FromSeconds(3))
+                .Until(drv => drv.FindElement(By.ClassName("new-todo")));
+
+            string modifiedTaskTitle = "modified";
+
+            newTaskInput.SendKeys("task1");
+            newTaskInput.SendKeys(Keys.Enter);
+
+            IWebElement taskList = new WebDriverWait(driver, TimeSpan.FromSeconds(3))
+                .Until(drv => drv.FindElement(By.ClassName("todo-list")));
+
+            IReadOnlyCollection<IWebElement> tasks = taskList.FindElements(By.TagName("li"));
+
+            IWebElement firstTask = tasks.First();
+
+            IWebElement firstTaskTitle = firstTask.FindElement(By.TagName("label"));
+
+            Actions actions = new Actions(driver);
+            firstTask.Click();
+            firstTask.Click();
+            actions.DoubleClick(firstTask).Perform();
+
+            IWebElement input = new WebDriverWait(driver, TimeSpan.FromSeconds(3))
+                .Until(drv => drv.FindElement(By.ClassName("edit")));
+
+            input.SendKeys(Keys.Control + "a");
+            input.SendKeys(Keys.Delete);
+
+            input.SendKeys(modifiedTaskTitle);
+            input.SendKeys(Keys.Enter);
+
+            Assert.Equal(modifiedTaskTitle, firstTaskTitle.Text);
         }
     }
 }
