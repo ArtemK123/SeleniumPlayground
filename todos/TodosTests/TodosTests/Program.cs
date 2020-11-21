4c49a03abad6a46eb2ae2e9a8c6be80fab2f923e
ArtemK123 <darkmode1012@gmail.com>
2020-11-21 15:49:23 +0200

    Added empty .net solution for tests

---
+++ todos/TodosTests/TodosTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodosTests
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
